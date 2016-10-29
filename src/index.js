cdd98382ffe17a8a11a8b5dc644fa06865f69dfe
Jeff Duke <jeff.duke@gmail.com>
2016-10-29 16:06:59 -0600

    Implement react router 4

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { render } from 'react-dom';
+import { BrowserRouter, Match} from 'react-router';
 import './styles/css/index.css';
+import App from './App';
+import Navigation from './Navigation';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Navigation />
+        <Match exactly pattern="/" component={App} />
+      </div>
+    </BrowserRouter>
+  )
+}
+
+render(<Root />, document.querySelector("#root"))
+
