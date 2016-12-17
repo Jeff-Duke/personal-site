7e14fbf82f55abfb28ba9a8ab28962323bc32d42
Jeff Duke <jeff.duke@gmail.com>
2016-12-17 16:20:39 -0700

    trying deployment with surge

--- src/Hero.js
+++ src/Hero.js
@@ -4,7 +4,7 @@ const Hero = () => {
   return(
     <article id="hero">
         <p className="hero-text">
-          Hi, I\'m Jeff and I like to write code that solves problems.
+          Hello world!  My name is Jeff and I like to write code that solves problems.
         </p>
     </article>
   )
