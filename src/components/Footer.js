6ab1edd1f7349dbd0331305c13deb39aad2e53e8
Jeff Duke <jeff.duke@gmail.com>
2016-12-30 19:16:43 -0700

    update styling

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return(
+        <div className="footer">
+            <a href="https://twitter.com/Jeff_Duke_io">Hello</a>
+        </div>
+    );
+}
+
+export default Footer;
